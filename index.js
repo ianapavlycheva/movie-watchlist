34709dab91554bf53579ee2ca8465471ceb9dc6f
ianapavlycheva <62348602+ianapavlycheva@users.noreply.github.com>
2024-11-30 18:11:44 +1100

    Remove index.js from version control

--- index.js
+++
@@ -1,8 +0,0 @@
-function handleClick() {
-  fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=1601530`)
-    .then((res) => res.json())
-    .then((data) => console.log(data))
-    .catch((err) => console.error("Error:", err));
-}
-
-document.getElementById("btn").addEventListener("click", handleClick);
